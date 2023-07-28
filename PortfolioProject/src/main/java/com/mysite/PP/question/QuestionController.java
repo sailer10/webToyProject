f86adace63c5a488577f4344dec87a500c9bbc29
seil. choi <sailer10@naver.com>
2023-07-28 13:25:54 +0900

    Completed to 3-14 searchBar

--- PortfolioProject/src/main/java/com/mysite/PP/question/QuestionController.java
+++ PortfolioProject/src/main/java/com/mysite/PP/question/QuestionController.java
@@ -35,12 +35,14 @@ public class QuestionController {
 	private final UserService userService;
 	
     @GetMapping("/list")
-    public String list(Model model, @RequestParam(value="page", defaultValue="0") int page) { 
+    public String list(Model model, @RequestParam(value="page", defaultValue="0") int page
+    		, @RequestParam(value = "kw", defaultValue = "") String kw) { 
     	// model 객체는 따로 생성할 필요없이 매개변수로 지정해두면
     	// 스프링부트가 자동으로 Model 객체를 생성함
-        Page<Question> paging = this.questionService.getList(page);
+    	// 검색어가 없다면 모든 질문내용을 리턴, 
+        Page<Question> paging = this.questionService.getList(page, kw);
         model.addAttribute("paging", paging);
-    	
+    	model.addAttribute("kw", kw);
     	// "question_list" 는 question_list.html 파일을 의미함
         return "question_list";
     }
--- PortfolioProject/src/main/java/com/mysite/PP/question/QuestionService.java
+++ PortfolioProject/src/main/java/com/mysite/PP/question/QuestionService.java
@@ -16,6 +16,15 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
 import lombok.RequiredArgsConstructor;
+
+import com.mysite.PP.answer.Answer;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.JoinType;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+import org.springframework.data.jpa.domain.Specification;
 /***
  * @author tpdlf
  * @summary @Service 객체는 모듈화, 보안을 위해 사용한다. 컨트롤러에서 직접 리포지터리를 호출하지 않고 service 를 중간에 두어 사용함.
@@ -26,6 +35,25 @@ public class QuestionService {
 
     private final QuestionRepository questionRepository;
 
+    // 자료 검색을 위한 메소드
+    private Specification<Question> search(String kw) {
+    	return new Specification<>() {
+    		private static final long serialVersionUID = 1L;
+    		@Override
+    		public Predicate toPredicate(Root<Question> q, CriteriaQuery<?> query, CriteriaBuilder cb) {
+    			query.distinct(true);
+    			Join<Question, SiteUser> u1 = q.join("author", JoinType.LEFT);
+    			Join<Question, Answer> a = q.join("answerList", JoinType.LEFT);
+    			Join<Answer, SiteUser> u2 = a.join("author", JoinType.LEFT);
+    			return cb.or(cb.like(q.get("subject"), "%" + kw + "%"),	// 제목
+    					cb.like(q.get("content"), "%" + kw + "%"),		// 내용
+    					cb.like(u1.get("username"), "%" + kw + "%"),    // 질문 작성자 
+    					cb.like(a.get("content"), "%" + kw + "%"),		// 답변 내용
+    					cb.like(u2.get("username"), "%" + kw + "%"));	// 답변 작성자
+    		}
+    	};
+    }    
+    
     public List<Question> getList() {
         return this.questionRepository.findAll();
     }
@@ -48,14 +76,20 @@ public class QuestionService {
     }
     
     
-    // 페이징을 위한 리스트 호출 메소드
-    public Page<Question> getList(int page) {
+    // 페이징을 위한 리스트 호출 메소드. 검색하여 찾거나 안하고 찾거나 이 메소드를 이용함
+    public Page<Question> getList(int page, String kw) {
     	// 최신 날짜순대로 보기위해 리스트 생성
     	List<Sort.Order> sorts = new ArrayList<>();
     	sorts.add(Sort.Order.desc("createDate"));
-    	// 이렇게 하면 해당 페이지의 데이터만 조회하도록 쿼리가 변경됨. 게시물 역숙조회를 위해 PageRequest.of의 3번째 파라미터로 Sort 객체를 전달함
+    	// 이렇게 하면 해당 페이지의 데이터만 조회하도록 쿼리가 변경됨. 게시물 역순조회를 위해 PageRequest.of의 3번째 파라미터로 Sort 객체를 전달함
         Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
-        return this.questionRepository.findAll(pageable);
+        // 검색어를 의미하는 매개변수 kw 를 추가함
+        Specification<Question> spec = search(kw);
+        
+        // 
+        //return this.questionRepository.findAll(spec, pageable);
+        // 쿼리문을 직접 작성하여 사용한다면 다음과 같이 리턴해 줘야한다.
+        return this.questionRepository.findAllByKeyword(kw, pageable);
     }
     
     public void modify(Question question, String subject, String content) {
@@ -73,4 +107,6 @@ public class QuestionService {
     	question.getVoter().add(siteUser);
     	this.questionRepository.save(question);
     }
+    
+    
 }
